f9c891173ca1a5da16b399b6297d85203f345f2f
kssumanth377@gmail.com <kssumanth377@gmail.com>
2024-06-11 23:18:53 +0530

    route modification

--- routes/userroute.js
+++ routes/userroute.js
@@ -6,13 +6,13 @@ const passport = require("passport");
 const { checkLogin } = require("../middleware.js");
 const router=express.Router();
 const userController=require("../controllers/users.js");
-router.route("/user/signup")
+router.route("/signup")
 .get(userController.renderSignUpForm)
 .post(wrapAsync(userController.doSignUp));
-router.route("/user/login")
+router.route("/login")
 .get(userController.renderLoginForm)
 .post(checkLogin,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),userController.doLogin);
-router.get("/user/logout",userController.doLogout);
+router.get("/logout",userController.doLogout);
 module.exports=router;
 // router.post("/login",passport.authenticate("local",{failureRedirect:"/login",failureFlash:true, successRedirect: '/listings'})
 // )  //if failure happens then redirect to login page only and flash a message
