24cc146a43efc7bdd9e285600be6b06ca95210b3
kssumanth377@gmail.com <kssumanth377@gmail.com>
2024-06-11 22:24:27 +0530

    removing home button

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use((req,res,next) => {
   next();
 }
 )
-
+app.use("/",listingsRouter);
 app.use("/listings",listingsRouter);
 app.use("/listings/:id/reviews",reviewsRouter);
 app.use("/",userRouter);
